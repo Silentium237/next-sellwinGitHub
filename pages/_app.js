992c05dd1235c95e652724105ad721ea99e0f58a
Гузюк Владимир <v.huziuk@sellwin.by>
2023-06-09 15:22:14 +0300

    4

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,23 @@
 import '../styles/global.css'
-import { Montserrat} from "@next/font/google";
-
-const roboto = Montserrat({
-    weight: '400',
-    subsets: ['latin'],
-});
+// import { Montserrat} from "@next/font/google";
+//
+// const roboto = Montserrat({
+//     weight: '400',
+//     subsets: ['latin'],
+// });
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
     return(
-        <main className={roboto.className}>
+        <>
             <Component {...pageProps} />
-        </main>
+            <style jsx global >
+                {`
+                body {
+                font-family: 'Montserrat' , sans-serif;
+                }`}
+            </style>
+        </>
 
         )
 
